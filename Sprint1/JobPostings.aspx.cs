abac1ed86793ac35076f4d14412e2c6b62562d68
LabPatron <labpatron@ad.jmu.edu>
2019-03-31 15:20:52 -0400

    new css and js

--- Sprint1/JobPostings.aspx.cs
+++ Sprint1/JobPostings.aspx.cs
@@ -13,33 +13,34 @@ public partial class JobPostings : System.Web.UI.Page
     SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["sqlCon"].ConnectionString);
 
     protected void Page_Load(object sender, EventArgs e)
-    {
-        TxtDatePosted.Text = DateTime.Now.ToString();
-    }
+    { }
+    //{
+    //    TxtDatePosted.Text = DateTime.Now.ToString();
+    //}
 
-    protected void Button1_Click(object sender, EventArgs e)
-    {
-        JobPostingsClass newPost = new JobPostingsClass(TxtJobTitle.Text, TxtCareerCluster.Text, TxtJobExplain.Value, TxtRequirements.Value, DateTime.Now);
+    //protected void Button1_Click(object sender, EventArgs e)
+    //{
+    //    JobPostingsClass newPost = new JobPostingsClass(TxtJobTitle.Text, TxtCareerCluster.Text, TxtJobExplain.Value, TxtRequirements.Value, DateTime.Now);
 
-        con.Open();
-        SqlCommand cmd = new SqlCommand("Insert into JobPostings values(@JobTitle, @CareerCluster, @JobExplanation, @Requirements, @DatePosted)", con);
-        cmd.CommandType = System.Data.CommandType.Text;
-        cmd.Connection = con;
+    //    con.Open();
+    //    SqlCommand cmd = new SqlCommand("Insert into JobPostings values(@JobTitle, @CareerCluster, @JobExplanation, @Requirements, @DatePosted)", con);
+    //    cmd.CommandType = System.Data.CommandType.Text;
+    //    cmd.Connection = con;
 
-        cmd.Parameters.AddWithValue("@JobTitle", newPost.GetJobTitle());
-        cmd.Parameters.AddWithValue("@CareerCluster", newPost.GetCareerCluster());
-        cmd.Parameters.AddWithValue("@JobExplanation", newPost.GetJobExplanation());
-        cmd.Parameters.AddWithValue("@Requirements", newPost.GetRequirements());
-        cmd.Parameters.AddWithValue("@DatePosted", newPost.GetPostedDate());
-        cmd.ExecuteNonQuery();
+    //    cmd.Parameters.AddWithValue("@JobTitle", newPost.GetJobTitle());
+    //    cmd.Parameters.AddWithValue("@CareerCluster", newPost.GetCareerCluster());
+    //    cmd.Parameters.AddWithValue("@JobExplanation", newPost.GetJobExplanation());
+    //    cmd.Parameters.AddWithValue("@Requirements", newPost.GetRequirements());
+    //    cmd.Parameters.AddWithValue("@DatePosted", newPost.GetPostedDate());
+    //    cmd.ExecuteNonQuery();
 
-        con.Close();
+    //    con.Close();
 
-    }
+    //}
 
-    protected void Back_Button_Click(object sender, EventArgs e)
-    {
-        Response.Redirect("BusinessPortal.aspx");
-        //Testing
-    }
+    //protected void Back_Button_Click(object sender, EventArgs e)
+    //{
+    //    Response.Redirect("BusinessPortal.aspx");
+    //    //Testing
+    //}
 }
