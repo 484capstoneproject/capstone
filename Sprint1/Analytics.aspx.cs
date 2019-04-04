c5d5ab24b635ad547c4547d5b3850ab67cda9b15
AustinC <ajchoi27@github.com>
2019-04-03 20:18:56 -0400

    Igor

--- Sprint1/Analytics.aspx.cs
+++ Sprint1/Analytics.aspx.cs
@@ -11,7 +11,7 @@ using System.Web.UI.WebControls;
 
 public partial class Analytics : System.Web.UI.Page
 {
-    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["sqlCon"].ConnectionString);
+   SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["sqlCon"].ConnectionString);
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -48,4 +48,83 @@ public partial class Analytics : System.Web.UI.Page
         FormsAuthentication.SignOut();
         FormsAuthentication.RedirectToLoginPage();
     }
+
+    public void ExportToExcel(object sender, EventArgs e)
+    {
+
+        //con.Open();
+        //SqlCommand cmd = new SqlCommand("select * from Business", con);
+        //cmd.CommandType = CommandType.Text;
+        ////cmd.Parameters.AddWithValue("@EntityID", LoginEntityID);
+        ////cmd.ExecuteNonQuery();
+
+        //SqlDataReader reader = cmd.ExecuteReader();
+        //String FirstName = "";
+
+        //if (reader.HasRows)
+        //{
+        //    while (reader.Read())
+        //    {
+
+        //        FirstName = reader["FirstName"].ToString();
+        //    }
+        //}
+        //reader.Close();
+        //con.Close();
+
+
+        //ExcelPackage pck = new ExcelPackage();
+        //ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Report");
+
+        ////ws.Cells["A1"].Value = "Communication";
+        ////ws.Cells["B1"].Value = "Com1";
+
+        //ws.Cells["A2"].Value = "Report";
+        //ws.Cells["B2"].Value = "Report1";
+
+        //ws.Cells["A3"].Value = "Date";
+        //ws.Cells["B3"].Value = string.Format("{0:dd MMMM yyyy} at {0:H: mm tt}", DateTimeOffset.Now);
+
+        //ws.Cells["A6"].Value = "[BusinessEntityID]";
+        //ws.Cells["B6"].Value = "[BusinessName]";
+        //ws.Cells["C6"].Value = "[EmailAddress]";
+        //ws.Cells["D6"].Value = "[PhoneNumber]";
+        //ws.Cells["E6"].Value = "[FirstName]";
+        //ws.Cells["E7"].Value = FirstName;
+
+        //int rowStart = 7;
+        //foreach (var item in emplist)
+        //{
+        //    if (item.Experience < 5)
+        //    {
+        //        ws.Row(rowStart).Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+        //        ws.Row(rowStart).Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml(string.Format("pink")));
+
+        //    }
+
+        //    ws.Cells[string.Format("A{0}", rowStart)].Value = item.EmployeeId;
+        //    ws.Cells[string.Format("B{0}", rowStart)].Value = item.EmployeeName;
+        //    ws.Cells[string.Format("C{0}", rowStart)].Value = item.Email;
+        //    ws.Cells[string.Format("D{0}", rowStart)].Value = item.Phone;
+        //    ws.Cells[string.Format("E{0}", rowStart)].Value = item.Experience;
+        //    rowStart++;
+        //}
+
+        //ws.Cells["A:AZ"].AutoFitColumns();
+        //Response.Clear();
+        //Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+        //Response.AddHeader("content-disposition", "attachment; filename=ExcelReport.xlsx");
+        //Response.BinaryWrite(pck.GetAsByteArray());
+        //Response.End();
+
+        Response.Clear();
+        Response.ContentType = "application/octet-stream";
+        Response.AddHeader("Content-Disposition", "attachment; filename=ExcelReport.xlsx");
+        Response.WriteFile(Server.MapPath("~/Excel/ExcelReport.xlsx"));
+        Response.End();
+
+    }
+
+
+
 }
--- Sprint1/LearningOpportunities.aspx
+++ Sprint1/LearningOpportunities.aspx
@@ -321,15 +321,12 @@
 							
 							 <asp:Gridview id="GridView1" 
                                autogeneratecolumns="False"
+                               autogenerateselectbutton="false"
                                onselectedindexchanged="GridView1_SelectedIndexChanged"
-                               runat="server" DataKeyNames="LearningPostingID" CellPadding="4" ForeColor="#333333" GridLines="None">
-                
-                                 <AlternatingRowStyle BackColor="White" />
+                               runat="server" DataKeyNames="LearningPostingID">
                 
                                  <Columns>
-                                     <asp:CommandField ShowSelectButton="True" SelectText="Update" ButtonType="Button" ControlStyle-Font-Bold="true">
-<ControlStyle Font-Bold="True"></ControlStyle>
-                                     </asp:CommandField>
+                                     <asp:CommandField ShowSelectButton="True" SelectText="Update" ButtonType="Button" ControlStyle-Font-Bold="true"/>
                                      <asp:BoundField DataField="LearningPostingID" 
                                          HeaderText="LearningPostingID" 
                                          InsertVisible="False" ReadOnly="True" 
@@ -361,19 +358,9 @@
                                          SortExpression="BusinessEntityID" 
                                          readOnly="true"/>
                                  </Columns> 
-                                    <EditRowStyle BackColor="#7C6F57" />
-                                 <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
-                                 <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
-                                 <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
-                                 <RowStyle BackColor="#E3EAEB" />
                                     <selectedrowstyle
-                                    forecolor="#333333"
-                                     font-bold="true" BackColor="#C5BBAF"/> 
-                
-                                 <SortedAscendingCellStyle BackColor="#F8FAFA" />
-                                 <SortedAscendingHeaderStyle BackColor="#246B61" />
-                                 <SortedDescendingCellStyle BackColor="#D4DFE1" />
-                                 <SortedDescendingHeaderStyle BackColor="#15524A" />
+                                    forecolor="#0066ff"
+                                     font-bold="true"/> 
                 
                              </asp:Gridview>              
                         
